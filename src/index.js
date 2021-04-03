1295d758029302998777eb2ba733f6dc875fb1ba
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-04-03 21:02:18 +0530

    Setup socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, '../public');
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+io.on("connection", () => {
+    console.log("New Connection!");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
