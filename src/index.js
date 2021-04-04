ee8332aa153e70b0dbd961ef73cc8c7c67f7ea8c
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-04-04 20:23:10 +0530

    Formatted code to single quotes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const http = require("http");
-const path = require("path");
-const socketio = require("socket.io");
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, "../public");
+const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", (socket) => {
-    console.log("New Connection!");
+io.on('connection', (socket) => {
+    console.log('New Connection!');
 
-    socket.emit("message", "Welcome!");
-    socket.broadcast.emit("message", "A new user has joined!");
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
 
-    socket.on("sendMessage", (message) => {
-        io.emit("message", message);
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
     });
 
-    socket.on("disconnect", () => {
-        io.emit("message", "A user has left!");
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!');
     });
 });
 
